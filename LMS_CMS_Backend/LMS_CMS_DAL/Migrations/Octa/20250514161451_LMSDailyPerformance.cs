b8b169d80d49c510096e5c27ef8b4665184cd3ba
mennausama <mennausama1682@gmail.com>
2025-05-14 19:41:18 +0300

    dailyperformance

--- LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Octa/20250514161451_LMSDailyPerformance.cs
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Octa/20250514161451_LMSDailyPerformance.cs
@@ -10,13 +10,27 @@ namespace LMS_CMS_DAL.Migrations.Octa
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.Sql(@"
+                INSERT INTO Page(ID, en_name, ar_name, enDisplayName_name, arDisplayName_name, Page_ID, IsDisplay) VALUES  
+                (192, 'Daily Performance Master Data', N'البيانات الرئيسية للأداء اليومي', 'Daily Performance Master Data', N'البيانات الرئيسية', 185, 1),
+                (193, 'Daily Performance Transaction', N'معاملات الأداء اليومي', 'Daily Performance Transaction', N'المعاملات', 185, 1),
+                (194, 'Daily Performance Reports', N'تقارير الأداء اليومي', 'Daily Performance Reports', N'التقارير', 185, 1),
 
+                (195, 'Enter Daily Performance', N'إدخال الأداء اليومي', 'Enter Daily Performance', N'إدخال الأداء اليومي', 193, 1),
+                (196, 'Student Medal', N'ميداليات الطلاب', 'Medal', N'الميداليات', 193, 1),
+
+                (197, 'Medal', N'الميداليات', 'Medal', N'الميداليات', 192, 1),
+
+                (198, 'Lesson Resources Types', N'أنواع موارد الدروس', 'Lesson Resources Types', N'أنواع موارد الدروس', 186, 1),
+                (199, 'Lesson Activity Types', N'أنواع أنشطة الدروس', 'Lesson Activity Types', N'أنواع أنشطة الدروس', 186, 1),
+                (200, 'Lessons', N'الدروس', 'Lessons', N'الدروس', 186, 1)
+            ");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql("DELETE FROM Page WHERE ID BETWEEN 192 AND 200");
         }
     }
 }
--- LMS_CMS_Frontend/src/app/Services/api.service.ts
+++ LMS_CMS_Frontend/src/app/Services/api.service.ts
@@ -18,7 +18,7 @@ export class ApiService {
   GetHeader(){    
     // const hostname = window.location.hostname;  
     // var Header = hostname.split('.')[0] 
-    var Header = "e"       
+    var Header = "d"       
 
     return Header;
   }
