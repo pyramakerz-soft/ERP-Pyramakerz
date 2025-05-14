d9ed09888ef102f509c5659b6c77c7f9bd8d6dfe
= <=>
2025-05-14 15:20:40 +0300

    Bugs

--- LMS_CMS_Backend/LMS_CMS_BL/DTO/Accounting/PayableMasterGetDTO.cs
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/Accounting/PayableMasterGetDTO.cs
@@ -9,7 +9,7 @@ namespace LMS_CMS_BL.DTO.Accounting
     public class PayableMasterGetDTO
     {
         public long ID { get; set; }
-        public int DocNumber { get; set; }
+        public string DocNumber { get; set; }
         public string Date { get; set; }
         public string Notes { get; set; }
         public long PayableDocTypeID { get; set; }
--- LMS_CMS_Frontend/src/app/Models/Accounting/accounting-entries.ts
+++ LMS_CMS_Frontend/src/app/Models/Accounting/accounting-entries.ts
@@ -1,7 +1,7 @@
 export class AccountingEntries {
     constructor(
         public id: number = 0,
-        public docNumber: number|null = null,
+        public docNumber: string|null = null,
         public date: string = '',
         public notes: string|null = null,
         public accountingEntriesDocTypeID: number = 0,
