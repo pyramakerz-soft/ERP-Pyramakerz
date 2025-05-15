55f10296f4f020d4357e2caa5ac7005c75166844
= <=>
2025-05-15 19:26:56 +0300

    National ID

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250515162034_EditNationalIDInCategoryFieldMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.Domains
+{
+    /// <inheritdoc />
+    public partial class EditNationalIDInCategoryFieldMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE CategoryField
+                SET EnName = 'National ID number'
+                WHERE ID IN (11, 17, 24);
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE CategoryField
+                SET EnName = 'ID No.'
+                WHERE ID IN (11, 17, 24);
+            ");
+        }
+    }
+}
