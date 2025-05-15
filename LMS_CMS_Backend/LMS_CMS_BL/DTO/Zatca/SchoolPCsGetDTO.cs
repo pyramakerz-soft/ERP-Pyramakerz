78311974e76d1a1d38db3140e60dbaf6ce589ba2
asalem.dev@gmail.com <asalem.dev@gmail.com>
2025-05-15 14:11:25 +0300

    add tax customer table

--- LMS_CMS_Backend/LMS_CMS_BL/DTO/Zatca/SchoolPCsGetDTO.cs
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/Zatca/SchoolPCsGetDTO.cs
@@ -5,6 +5,7 @@
         public long ID { get; set; }
         public string PCName { get; set; }
         public string SerialNumber { get; set; }
+        public long SchoolId { get; set; }
         public string School { get; set; }
         public DateOnly? CertificateDate { get; set; }
     }
