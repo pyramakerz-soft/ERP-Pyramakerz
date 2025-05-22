af6e65d1d069107b4811cf7dec7e9e92d723726c
Abdelrahman Salem <asalem.dev@gmail.com>
2025-05-22 12:57:44 +0300

    update

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250522094947_AdLongIdToMaster.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.LMS_CMS_
+{
+    /// <inheritdoc />
+    public partial class AdLongIdToMaster : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShareLongId",
+                table: "InventoryMaster",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShareLongId",
+                table: "InventoryMaster");
+        }
+    }
+}
