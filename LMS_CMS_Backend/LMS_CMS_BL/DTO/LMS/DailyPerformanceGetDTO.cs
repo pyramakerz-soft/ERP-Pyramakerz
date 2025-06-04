24487953b6faad640865c4ed5d031443edb4aab6
mennausama <mennausama1682@gmail.com>
2025-06-04 13:55:01 +0300

    dailyperformanceMaster

--- LMS_CMS_Backend/LMS_CMS_BL/DTO/LMS/DailyPerformanceGetDTO.cs
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/LMS/DailyPerformanceGetDTO.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace LMS_CMS_BL.DTO.LMS
 {
-    internal class DailyPerformanceGetDTO
+    public class DailyPerformanceGetDTO
     {
+        public long ID { get; set; }
+        public string? Comment { get; set; }
+        public long SubjectID { get; set; }
+        public long StudentID { get; set; }
+        public long GradeID { get; set; }
+        public string SubjectName { get; set; }
+        public string StudentName { get; set; }
+        public string GradeName { get; set; }
+        public List<StudentPerformanceAddDTO> StudentPerformance { get; set; }
     }
 }
