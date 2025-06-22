b853c7c31ab362af403681d33f293472f6c82964
= <=>
2025-06-22 15:45:50 +0300

    Subj

---
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/LMS/StudentClassWhenSubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_BL.DTO.LMS
+{
+    public class StudentClassWhenSubject
+    {  
+        public long ClassroomID { get; set; }
+        public string ClassroomName { get; set; }
+        public List<StudentClassroomGetDTO> StudentClassrooms { get; set; } = new List<StudentClassroomGetDTO>();
+    }
+}
