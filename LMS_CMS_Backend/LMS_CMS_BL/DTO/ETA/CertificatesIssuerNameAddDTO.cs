73deb5d649575509c1c3418ddf7f54f893dcaa1c
Abdelrahman Salem <asalem.dev@gmail.com>
2025-06-25 10:06:11 +0300

    updates

--- LMS_CMS_Backend/LMS_CMS_BL/DTO/ETA/CertificatesIssuerNameAddDTO.cs
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/ETA/CertificatesIssuerNameAddDTO.cs
@@ -3,7 +3,5 @@
     public class CertificatesIssuerNameAddDTO
     {
         public string Name { get; set; }
-        public string? UserName { get; set; }
-        public string? MyGetDate { get; set; }
     }
 }
--- LMS_CMS_Backend/LMS_CMS_BL/DTO/ETA/CertificatesIssuerNameEditDTO.cs
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/ETA/CertificatesIssuerNameEditDTO.cs
@@ -5,7 +5,5 @@ namespace LMS_CMS_BL.DTO.ETA
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string? UserName { get; set; }
-        public string? MyGetDate { get; set; }
     }
 }
