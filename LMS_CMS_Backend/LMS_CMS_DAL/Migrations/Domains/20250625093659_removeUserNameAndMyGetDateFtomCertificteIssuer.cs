658469db8dcf73b641fa225aa4962fbafa134d34
Abdelrahman Salem <asalem.dev@gmail.com>
2025-06-25 12:42:31 +0300

    updates

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250625093659_removeUserNameAndMyGetDateFtomCertificteIssuer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.LMS_CMS_
+{
+    /// <inheritdoc />
+    public partial class removeUserNameAndMyGetDateFtomCertificteIssuer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyGetDate",
+                table: "CertificatesIssuerNames");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "CertificatesIssuerNames");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MyGetDate",
+                table: "CertificatesIssuerNames",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "CertificatesIssuerNames",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
