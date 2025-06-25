36564f4f1f7c9acfc9ee373f8e3ef4e3c0c330c1
= <=>
2025-06-25 11:49:20 +0300

    Conflict

--- LMS_CMS_Frontend/src/app/Models/ETA/certificates-issuer.ts
+++ LMS_CMS_Frontend/src/app/Models/ETA/certificates-issuer.ts
@@ -2,6 +2,7 @@ export class CertificatesIssuer {
     constructor(
         public id: number = 0, 
         public name: string = '',  
+        public insertedAt: string = '',  
         public insertedByUserId :number =0,
     ) {}
 }
