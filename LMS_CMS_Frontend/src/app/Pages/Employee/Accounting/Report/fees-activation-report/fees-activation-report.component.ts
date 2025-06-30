11d6633bd40a191947dc20e522799386d518d1ba
= <=>
2025-06-30 13:51:41 +0300

    Report

---
+++ LMS_CMS_Frontend/src/app/Pages/Employee/Accounting/Report/fees-activation-report/fees-activation-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fees-activation-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './fees-activation-report.component.html',
+  styleUrl: './fees-activation-report.component.css'
+})
+export class FeesActivationReportComponent {
+
+}
