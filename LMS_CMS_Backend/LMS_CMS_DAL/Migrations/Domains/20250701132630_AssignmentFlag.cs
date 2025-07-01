4bcc2e0bd30a3cda848a07690412cb9bc1bfe361
mennausama <mennausama1682@gmail.com>
2025-07-01 16:28:17 +0300

    migration

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250701132630_AssignmentFlag.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.Domains
+{
+    /// <inheritdoc />
+    public partial class AssignmentFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "LessonLive",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsVisibleToStudent",
+                table: "Assignment",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "LessonLive");
+
+            migrationBuilder.DropColumn(
+                name: "IsVisibleToStudent",
+                table: "Assignment");
+        }
+    }
+}
