adacaefc72d1455bf3e94f726d2fc7db4c0ba4c1
Abdelrahman Salem <asalem.dev@gmail.com>
2025-07-09 13:26:50 +0300

    updates

--- LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250709101741_AddGetSubAccNameScript.cs
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250709101741_AddGetSubAccNameScript.cs
@@ -5,32 +5,32 @@
 namespace LMS_CMS_DAL.Migrations.LMS_CMS_
 {
     /// <inheritdoc />
-    public partial class AddGetSubAccScript : Migration
+    public partial class AddGetSubAccNameScript : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var assembly = typeof(AddGetSubAccScript).Assembly;
-            var filterEntries = "LMS_CMS_DAL.DbScripts.FilterEntries.sql"; // Use actual namespace path
-
-            using var feStream = assembly.GetManifestResourceStream(filterEntries);
-            using var feReader = new StreamReader(feStream);
-            var sql = feReader.ReadToEnd();
-            migrationBuilder.Sql(sql);
-
+            var assembly = typeof(AddGetSubAccNameScript).Assembly;
             var subAccName = "LMS_CMS_DAL.DbScripts.GetSubAccountName.sql"; // Use actual namespace path
 
             using var saStream = assembly.GetManifestResourceStream(subAccName);
             using var saReader = new StreamReader(saStream);
-            sql = saReader.ReadToEnd();
+            var sql = saReader.ReadToEnd();
+            migrationBuilder.Sql(sql);
+
+            var filterEntries = "LMS_CMS_DAL.DbScripts.FilterEntries.sql"; // Use actual namespace path
+
+            using var feStream = assembly.GetManifestResourceStream(filterEntries);
+            using var feReader = new StreamReader(feStream);
+            sql = feReader.ReadToEnd();
             migrationBuilder.Sql(sql);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.Sql("DROP FUNCTION IF EXISTS [dbo].[FilterEntries];");
             migrationBuilder.Sql("DROP FUNCTION IF EXISTS [dbo].[GetSubAccountName];");
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS [dbo].[FilterEntries];");
         }
     }
 }
