a81291092605d29fe4f0aec21f8242adbe5cfb07
Abdelrahman Salem <asalem.dev@gmail.com>
2025-07-09 12:51:23 +0300

    create GetSubAccName in sql and add the script to the project

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250709094215_AddGetSubAccScript.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.LMS_CMS_
+{
+    /// <inheritdoc />
+    public partial class AddGetSubAccScript : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var assembly = typeof(AddGetSubAccScript).Assembly;
+            var filterEntries = "LMS_CMS_DAL.DbScripts.FilterEntries.sql"; // Use actual namespace path
+
+            using var feStream = assembly.GetManifestResourceStream(filterEntries);
+            using var feReader = new StreamReader(feStream);
+            var sql = feReader.ReadToEnd();
+            migrationBuilder.Sql(sql);
+
+            var subAccName = "LMS_CMS_DAL.DbScripts.GetSubAccountName.sql"; // Use actual namespace path
+
+            using var saStream = assembly.GetManifestResourceStream(subAccName);
+            using var saReader = new StreamReader(saStream);
+            sql = saReader.ReadToEnd();
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS [dbo].[FilterEntries];");
+            migrationBuilder.Sql("DROP FUNCTION IF EXISTS [dbo].[GetSubAccountName];");
+        }
+    }
+}
