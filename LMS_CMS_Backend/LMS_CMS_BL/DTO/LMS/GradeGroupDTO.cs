dba45ca9a95c51d71a272feb2156e561f467298a
mennausama <mennausama1682@gmail.com>
2025-07-10 11:59:35 +0300

    date time

---
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/LMS/GradeGroupDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS_CMS_BL.DTO.LMS
+{
+    public class GradeGroupDTO
+    {
+        public long GradeId { get; set; }
+        public string GradeName { get; set; }
+        public List<ClassroomGroupDTO> Classrooms { get; set; }
+    }
+}
