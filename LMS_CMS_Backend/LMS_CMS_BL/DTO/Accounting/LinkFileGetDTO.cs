b1f27afae9a139842b7cd6a8516f715deaff5d7e
Abdelrahman Salem <asalem.dev@gmail.com>
2025-07-17 13:04:45 +0300

    Add AcountType to LinkFile

--- LMS_CMS_Backend/LMS_CMS_BL/DTO/Accounting/LinkFileGetDTO.cs
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/Accounting/LinkFileGetDTO.cs
@@ -12,5 +12,6 @@ namespace LMS_CMS_BL.DTO.Accounting
         public long ID { get; set; }
         public string Name { get; set; }
         public string ArName { get; set; }
+        public string? AccountType { get; set; }
     }
 }
