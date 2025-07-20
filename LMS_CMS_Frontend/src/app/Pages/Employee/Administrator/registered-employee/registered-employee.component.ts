1df5fb60c508fe9cfeb2a6315b677eff7d2167a3
= <=>
2025-07-20 22:04:35 +0300

    Regist4er

---
+++ LMS_CMS_Frontend/src/app/Pages/Employee/Administrator/registered-employee/registered-employee.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registered-employee',
+  standalone: true,
+  imports: [],
+  templateUrl: './registered-employee.component.html',
+  styleUrl: './registered-employee.component.css'
+})
+export class RegisteredEmployeeComponent {
+
+}
