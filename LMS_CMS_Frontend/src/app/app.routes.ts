6730ba1191ad9ba37b01e20cc1f178955b919bf7
Seif-shawky <bsyco006@gmail.com>
2025-08-06 16:14:47 +0300

    fix bugs in inventory

--- LMS_CMS_Frontend/src/app/app.routes.ts
+++ LMS_CMS_Frontend/src/app/app.routes.ts
@@ -230,8 +230,6 @@ export const routes: Routes = [
             { path: "Hygiene Form Medical Report", component: HygieneFormComponent, title: "Hygiene Form" },
             { path: "Create Hygiene Form", component: CreateHygieneFormComponent, title: "Create Hygiene Form" },
             { path: 'view hygiene form/:id', component: ViewHygieneFormComponent },
-            // { path: 'mh by parent/:id', component: MedicalHistoryByParentComponent },
-            // { path: 'mh by doctor/:id', component: MedicalHistoryByDoctorComponent },
             { path: "Follow Up", component: FollowUpComponent, title: "Follow Up" },
             { path: "Medical History", component: MedicalHistoryComponent, title: "Medical History" },
             { path: "Medical Report", component: MedicalReportComponent, title: "Medical Report" },
