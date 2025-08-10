20a164734db49e5852fc6a827f519e3ab84bc9bd
mennausama <mennausama1682@gmail.com>
2025-08-10 14:20:22 +0300

    conduct

---
+++ LMS_CMS_Backend/LMS_CMS/Controllers/Domains/SocialWorker/ConductTypeController.cs
@@ -0,0 +1,140 @@
+﻿using AutoMapper;
+using LMS_CMS_BL.DTO.SocialWorker;
+using LMS_CMS_BL.UOW;
+using LMS_CMS_DAL.Models.Domains.SocialWorker;
+using LMS_CMS_PL.Attribute;
+using LMS_CMS_PL.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LMS_CMS_PL.Controllers.Domains.SocialWorker
+{
+    [Route("api/with-domain/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ConductTypeController : ControllerBase
+    {
+        private readonly DbContextFactoryService _dbContextFactory;
+        IMapper mapper;
+        private readonly CheckPageAccessService _checkPageAccessService;
+
+        public ConductTypeController(DbContextFactoryService dbContextFactory, IMapper mapper, CheckPageAccessService checkPageAccessService)
+        {
+            _dbContextFactory = dbContextFactory;
+            this.mapper = mapper;
+            _checkPageAccessService = checkPageAccessService;
+        }
+
+        ////////////////////////////////
+        
+        [HttpGet]
+        [Authorize_Endpoint_(
+            allowedTypes: new[] { "octa", "employee" },
+            pages: new[] { "Lesson Resources Types" }
+        )]
+        public IActionResult Get()
+        {
+            UOW Unit_Of_Work = _dbContextFactory.CreateOneDbContext(HttpContext);
+
+            var userClaims = HttpContext.User.Claims;
+            var userIdClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "id")?.Value;
+            long.TryParse(userIdClaim, out long userId);
+            var userTypeClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "type")?.Value;
+
+            if (userIdClaim == null || userTypeClaim == null)
+            {
+                return Unauthorized("User ID or Type claim not found.");
+            }
+
+            List<ConductType> conductTypes = Unit_Of_Work.conductType_Repository.FindBy(t => t.IsDeleted != true);
+
+            if (conductTypes == null || conductTypes.Count == 0)
+            {
+                return NotFound();
+            }
+
+            List<ConductTypeGetDTO> Dto = mapper.Map<List<ConductTypeGetDTO>>(conductTypes);
+
+            return Ok(Dto);
+        }
+
+        ////////////////////////////////
+
+        [HttpGet("{id}")]
+        [Authorize_Endpoint_(
+           allowedTypes: new[] { "octa", "employee" },
+           pages: new[] { "Lesson Resources Types" }
+         )]
+        public async Task<IActionResult> GetById(long id)
+        {
+            UOW Unit_Of_Work = _dbContextFactory.CreateOneDbContext(HttpContext);
+
+            var userClaims = HttpContext.User.Claims;
+            var userIdClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "id")?.Value;
+            long.TryParse(userIdClaim, out long userId);
+            var userTypeClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "type")?.Value;
+
+            if (userIdClaim == null || userTypeClaim == null)
+            {
+                return Unauthorized("User ID or Type claim not found.");
+            }
+
+            ConductType conductType = Unit_Of_Work.conductType_Repository.First_Or_Default(sem => sem.IsDeleted != true && sem.ID == id);
+
+            if (conductType == null)
+            {
+                return NotFound();
+            }
+
+            ConductTypeGetDTO Dto = mapper.Map<ConductTypeGetDTO>(conductType);
+
+            return Ok(Dto);
+        }
+
+        ////////////////////////////////
+
+        [HttpPost]
+        [Authorize_Endpoint_(
+         allowedTypes: new[] { "octa", "employee" },
+         pages: new[] { "Lesson Resources Types" }
+        )]
+        public async Task<IActionResult> Add(ConductTypeAddDTO NewConduct)
+        {
+            UOW Unit_Of_Work = _dbContextFactory.CreateOneDbContext(HttpContext);
+
+            var userClaims = HttpContext.User.Claims;
+            var userIdClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "id")?.Value;
+            long.TryParse(userIdClaim, out long userId);
+            var userTypeClaim = HttpContext.User.Claims.FirstOrDefault(c => c.Type == "type")?.Value;
+
+            if (userIdClaim == null || userTypeClaim == null)
+            {
+                return Unauthorized("User ID or Type claim not found.");
+            }
+            if (NewConduct == null)
+            {
+                return BadRequest("Conduct is empty");
+            }
+
+            ConductType conduct = mapper.Map<ConductType>(NewConduct);
+
+            TimeZoneInfo cairoZone = TimeZoneInfo.FindSystemTimeZoneById("Egypt Standard Time");
+            conduct.InsertedAt = TimeZoneInfo.ConvertTime(DateTime.Now, cairoZone);
+            if (userTypeClaim == "octa")
+            {
+                conduct.InsertedByOctaId = userId;
+            }
+            else if (userTypeClaim == "employee")
+            {
+                conduct.InsertedByUserId = userId;
+            }
+            Unit_Of_Work.conductType_Repository.Add(conduct);
+            Unit_Of_Work.SaveChanges();
+            return Ok(NewConduct);
+        }
+
+        ////////////////////////////////
+
+    }
+}
