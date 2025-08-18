642b3088187bea7bdf0e69c5c85b19795cfdf1df
Abdelrahman Salem <asalem.dev@gmail.com>
2025-08-18 23:02:30 +0300

    updates

--- LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/AccountingModule/Reports/AccountingEntriesReport.cs
+++ LMS_CMS_Backend/LMS_CMS_DAL/Models/Domains/AccountingModule/Reports/AccountingEntriesReport.cs
@@ -6,8 +6,8 @@
         public long? DetailsID { get; set; }
         public long? AccountID { get; set; }
         public long? Serial { get; set; }
-        //public long? MainAccountNo { get; set; }
-        //public string? MainAccount { get; set; }
+        public long? MainAccountNo { get; set; }
+        public string? MainAccount { get; set; }
         public long? SubAccountNo { get; set; }
         public string? SubAccount { get; set; }
         public decimal? Debit { get; set; }
