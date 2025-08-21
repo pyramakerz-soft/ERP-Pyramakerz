131a9e96b2a02e46a8fea1ce51e2ab14fe40c67e
Abdelrahman Salem <asalem.dev@gmail.com>
2025-08-21 13:11:10 +0300

    adding indexes on accounting tables

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250821100851_AddIndexesOnAccountingTables.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.LMS_CMS_
+{
+    /// <inheritdoc />
+    public partial class AddIndexesOnAccountingTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceivableMaster_BankOrSaveID",
+                table: "ReceivableMaster",
+                column: "BankOrSaveID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceivableMaster_Date",
+                table: "ReceivableMaster",
+                column: "Date");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceivableMaster_IsDeleted",
+                table: "ReceivableMaster",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceivableDetails_IsDeleted",
+                table: "ReceivableDetails",
+                column: "IsDeleted");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ReceivableDetails_LinkFileTypeID",
+                table: "ReceivableDetails",
+                column: "LinkFileTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PayableMaster_BankOrSaveID",
+                table: "PayableMaster",
+                column: "BankOrSaveID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PayableMaster_Date",
+                table: "PayableMaster",
+                column: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ReceivableMaster_BankOrSaveID",
+                table: "ReceivableMaster");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ReceivableMaster_Date",
+                table: "ReceivableMaster");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ReceivableMaster_IsDeleted",
+                table: "ReceivableMaster");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ReceivableDetails_IsDeleted",
+                table: "ReceivableDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ReceivableDetails_LinkFileTypeID",
+                table: "ReceivableDetails");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PayableMaster_BankOrSaveID",
+                table: "PayableMaster");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PayableMaster_Date",
+                table: "PayableMaster");
+        }
+    }
+}
