bdf026bb6415999f3663fd4721d29afc04b63b76
Abdelrahman Salem <asalem.dev@gmail.com>
2025-08-25 10:06:47 +0300

    updae reports

--- LMS_CMS_Frontend/src/app/Services/api.service.ts
+++ LMS_CMS_Frontend/src/app/Services/api.service.ts
@@ -4,9 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiService {
-  // BaseUrl = 'https://localhost:7205/api/with-domain';
-  // BaseUrlOcta = 'https://localhost:7205/api';
-  // BaseUrlSignalR = 'https://localhost:7205/';
+  BaseUrl = 'https://localhost:7205/api/with-domain';
+  BaseUrlOcta = 'https://localhost:7205/api';
+  BaseUrlSignalR = 'https://localhost:7205/';
 
   // BaseUrl = 'http://localhost:5094/api/with-domain';
   // BaseUrlOcta = 'http://localhost:5094/api';
@@ -19,10 +19,10 @@ export class ApiService {
   constructor() {}
 
   GetHeader() {
-    // const hostname = window.location.hostname; 
-    // var Header = hostname.split('.')[0] 
-    
-    var Header = 'try';    
+    // const hostname = window.location.hostname;
+    // var Header = hostname.split('.')[0]
+
+    var Header = 'xx';
 
     return Header;
   }
