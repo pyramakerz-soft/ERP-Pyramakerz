82f5b285e25ee6477463e7f73d162f2eb36797cb
Seif-shawky <bsyco006@gmail.com>
2025-09-03 11:14:24 +0300

    .

--- LMS_CMS_Backend/LMS_CMS/Services/GetConnectionStringService.cs
+++ LMS_CMS_Backend/LMS_CMS/Services/GetConnectionStringService.cs
@@ -16,7 +16,7 @@
 
         public string BuildConnectionString(string domainName)
         { 
-            var dataSource = "."; 
+            var dataSource = "SW-13\\SQLEXPRESS"; 
             var initialCatalog = domainName;
 
             return $"Data Source={dataSource};Initial Catalog={initialCatalog};Integrated Security = True;TrustServerCertificate=True";
