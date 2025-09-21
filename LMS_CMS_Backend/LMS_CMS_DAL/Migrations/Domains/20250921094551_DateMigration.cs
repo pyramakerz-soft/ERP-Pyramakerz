6599cdc7baf0d6e9ede4fb9f2d9301ca53f381e8
= <=>
2025-09-21 12:53:58 +0300

    Migration

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20250921094551_DateMigration.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.Domains
+{
+    /// <inheritdoc />
+    public partial class DateMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF EXISTS (
+                    SELECT * 
+                    FROM INFORMATION_SCHEMA.COLUMNS 
+                    WHERE TABLE_NAME = 'Employee' AND COLUMN_NAME = 'HireDate' 
+                    AND DATA_TYPE = 'nvarchar')  -- This assumes the column type is string (nvarchar)
+                BEGIN
+                    ALTER TABLE Employee 
+                    ALTER COLUMN HireDate DATE;
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF EXISTS (
+                    SELECT * 
+                    FROM INFORMATION_SCHEMA.COLUMNS 
+                    WHERE TABLE_NAME = 'Employee' AND COLUMN_NAME = 'HireDate' 
+                    AND DATA_TYPE = 'date')  -- Checking if it's already Date
+                BEGIN
+                    ALTER TABLE Employee 
+                    ALTER COLUMN HireDate NVARCHAR(255);  -- Reverting to string
+                END
+            ");
+        }
+    }
+}
