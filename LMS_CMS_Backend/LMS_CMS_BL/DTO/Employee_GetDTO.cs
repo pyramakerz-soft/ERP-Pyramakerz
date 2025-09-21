03b05605f337a696ce61578000f5b60df19aa2db
= <=>
2025-09-21 11:23:27 +0300

    Employee Pass

--- LMS_CMS_Backend/LMS_CMS_BL/DTO/Employee_GetDTO.cs
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/Employee_GetDTO.cs
@@ -24,9 +24,7 @@ namespace LMS_CMS_BL.DTO
 
         [StringLength(100, ErrorMessage = "لا يمكن أن يكون الاسم أطول من 100 حرف")]
         public string? ar_name { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
-        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be between 6 and 100 characters.")]
+         
         public string? Mobile { get; set; }
         public string? Phone { get; set; }
         public string? Email { get; set; }
