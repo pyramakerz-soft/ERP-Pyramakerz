db06c98c2c31a7ed162dc6c1892366789ba1f312
Seif-shawky <bsyco006@gmail.com>
2025-09-22 14:02:33 +0300

    loans-report / assignment-report fix

---
+++ LMS_CMS_Frontend/src/app/Pages/Employee/HR/Reports/loans-report/loans-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loans-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './loans-report.component.html',
+  styleUrl: './loans-report.component.css'
+})
+export class LoansReportComponent {
+
+}
