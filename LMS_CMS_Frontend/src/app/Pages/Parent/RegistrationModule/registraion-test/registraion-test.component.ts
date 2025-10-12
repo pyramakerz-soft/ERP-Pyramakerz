d44c3b9d9daea63a714abfc7ef4271d6116d09f4
menna osama <menna osama@gmail.com>
2025-10-12 12:49:22 +0300

    Revert "0bugs"
    This reverts commit 2c12d0fbdf3707f7855107f8fb5b7cc4357b527a.

--- LMS_CMS_Frontend/src/app/Pages/Parent/RegistrationModule/registraion-test/registraion-test.component.ts
+++ LMS_CMS_Frontend/src/app/Pages/Parent/RegistrationModule/registraion-test/registraion-test.component.ts
@@ -217,7 +217,7 @@ export class RegistraionTestComponent {
   }
 
   checkForTestAvailabilityForParent() {
-    this.registerationFormParentService.GetById(this.registerationFormParentID, this.DomainName).subscribe(  // this will check if this registerationFormParentID can access this test
+    this.registerationFormParentService.GetById(this.registerationFormParentID, this.DomainName).subscribe(
       registrationParent => {
         this.testService.GetByID(this.TestId, this.DomainName).subscribe(
           test => {
@@ -226,8 +226,6 @@ export class RegistraionTestComponent {
             }
           }
         )
-      },error=>{
-       this.moveToTest()
       }
     )
 
