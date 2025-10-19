1e2949937aae3dd08b586f520c1a0d07eb3cb185
= <=>
2025-10-19 17:05:11 +0300

    Dashboard

--- LMS_CMS_Backend/LMS_CMS/Services/Dashboard/ECommerce_Service.cs
+++ LMS_CMS_Backend/LMS_CMS/Services/Dashboard/ECommerce_Service.cs
@@ -1,6 +1,31 @@
-﻿namespace LMS_CMS_PL.Services.Dashboard
+﻿using LMS_CMS_DAL.Models.Domains.Inventory;
+
+namespace LMS_CMS_PL.Services.Dashboard
 {
     public class ECommerce_Service
     {
+        private readonly DbContextFactoryService _dbContextFactory;
+
+        public ECommerce_Service(DbContextFactoryService dbContextFactory)
+        {
+            _dbContextFactory = dbContextFactory;
+        }
+
+        //public int TotalOrders(int year, int? month, HttpContext httpContext)
+        //{
+        //    var unitOfWork = _dbContextFactory.CreateOneDbContext(httpContext);
+
+        //    List<ShopItem> shopItems= unitOfWork.shopItem_Repository
+        //        .FindBy(f =>
+        //            f.IsDeleted != true && 
+        //            f.Date.Year == year &&
+        //            (month == null || f.Date.Month == month)
+        //        ).ToList();
+
+        //    Dictionary<string, decimal> result = new Dictionary<string, decimal>();
+             
+
+        //    return result;
+        //}
     }
 }
