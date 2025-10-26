54e9ed76c1ea03a6a266a7a0e8a2be9ba4e08a0f
Gaber-7 <gaberalkholy053@gmail.com>
2025-10-26 08:58:34 +0000

    ClassroomDailyPerformanceAverages

--- LMS_CMS_Backend/LMS_CMS_BL/DTO/LMS/ClassroomDailyPerformanceAverageDTO.cs
+++ LMS_CMS_Backend/LMS_CMS_BL/DTO/LMS/ClassroomDailyPerformanceAverageDTO.cs
@@ -9,15 +9,14 @@ namespace LMS_CMS_BL.DTO.LMS
     public class ClassroomDailyPerformanceAverageDTO
     {
         public DateOnly Date { get; set; }
-        public string ClassroomName { get; set; }
-        public string Comment { get; set; }
-        public List<StudentPerformanceDTO> StudentPerformance { get; set; }
+        public string ClassroomName { get; set; } = string.Empty;
+        public List<StudentPerformanceDTO> StudentPerformance { get; set; } = new();
     }
     public class StudentPerformanceDTO
     {
         public long PerformanceTypeID { get; set; }
-        public string PerformanceTypeEn { get; set; }
-        public string PerformanceTypeAr { get; set; }
+        public string PerformanceTypeEn { get; set; } = string.Empty;
+        public string PerformanceTypeAr { get; set; } = string.Empty;
         public double AverageScore { get; set; }
     }
 }
