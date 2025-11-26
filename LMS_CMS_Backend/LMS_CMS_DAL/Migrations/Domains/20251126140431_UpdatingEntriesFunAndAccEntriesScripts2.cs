5e42dbc150c8497d22896ed5bc6c6ca7c19217f4
Abdelrahman Salem <asalem.dev@gmail.com>
2025-11-26 16:12:36 +0200

    update database scripts

---
+++ LMS_CMS_Backend/LMS_CMS_DAL/Migrations/Domains/20251126140431_UpdatingEntriesFunAndAccEntriesScripts2.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LMS_CMS_DAL.Migrations.LMS_CMS_
+{
+    /// <inheritdoc />
+    public partial class UpdatingEntriesFunAndAccEntriesScripts2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            ExecuteEmbeddedSql(migrationBuilder, "LMS_CMS_DAL.DbScripts.EntriesFunV04.sql");
+            ExecuteEmbeddedSql(migrationBuilder, "LMS_CMS_DAL.DbScripts.GetAccountingEntriesV05.sql");
+        }
+
+        private void ExecuteEmbeddedSql(MigrationBuilder migrationBuilder, string resourceName)
+        {
+            var assembly = typeof(UpdatingEntriesFunAndAccEntriesScripts2).Assembly;
+
+            using var stream = assembly.GetManifestResourceStream(resourceName)
+                ?? throw new FileNotFoundException($"Embedded SQL script not found: {resourceName}");
+
+            using var reader = new StreamReader(stream);
+            var sql = reader.ReadToEnd();
+
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
