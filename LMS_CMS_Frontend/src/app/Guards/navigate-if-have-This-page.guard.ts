96af765f385b7267bb677d65da22eaf31654d003
= <=>
2025-12-09 17:01:25 +0200

    t

--- LMS_CMS_Frontend/src/app/Guards/navigate-if-have-This-page.guard.ts
+++ LMS_CMS_Frontend/src/app/Guards/navigate-if-have-This-page.guard.ts
@@ -72,16 +72,10 @@ export const navigateIfHaveSettingPageGuard: CanActivateFn = (route, state) => {
     return of(false);
   }
 
-  // const rawPath = route.routeConfig?.path || '';
-  // const firstSegment = rawPath.split('/')[0];
-  // const pageName = decodeURIComponent(firstSegment);
-  const pageName = route.routeConfig?.data?.['pageName'];
+  const rawPath = route.routeConfig?.path || '';
+  const firstSegment = rawPath.split('/')[0];
+  const pageName = decodeURIComponent(firstSegment);
 
-  if (!pageName) {
-    router.navigateByUrl('');
-    return of(false);
-  }
-  
   return roleDetailsService.CheckPageAccess(userData.role, pageName).pipe(
     map(() => true),
 
