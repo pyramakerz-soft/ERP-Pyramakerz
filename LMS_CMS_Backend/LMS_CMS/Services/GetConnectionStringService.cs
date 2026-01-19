f7751d5fe2f6fcea6d162f9a4cd516170fcad0ce
Moakramm <ma987236@gmail.com>
2026-01-19 14:04:48 +0200

    hello brother

--- LMS_CMS_Backend/LMS_CMS/Services/GetConnectionStringService.cs
+++ LMS_CMS_Backend/LMS_CMS/Services/GetConnectionStringService.cs
@@ -3,26 +3,26 @@
     public class GetConnectionStringService
     {
 
-        //public string BuildConnectionString(string domainName)
-        //{ 
-        //    var dataSource = "octa-db.cxg0g2422n2v.us-east-1.rds.amazonaws.com,1433";
-        //    var initialCatalog = domainName; 
-        //    var userId = "admin";
-        //    var password = "PyraDev1*";
-        //    var trustServerCertificate = "TrustServerCertificate=True
+        public string BuildConnectionString(string domainName)
+        { 
+           var dataSource = "awseb-e-z2aaakjmbk-stack-awsebrdsdatabase-dhhclswif7dt.cxg0g2422n2v.us-east-1.rds.amazonaws.com,1433";
+           var initialCatalog = domainName; 
+           var userId = "pyraerp";
+           var password = "erppyra*";
+           var trustServerCertificate = "TrustServerCertificate=True";
 
 
-        //    return $"Data Source={dataSource};Initial Catalog={initialCatalog};User ID={userId};Password={password};{trustServerCertificate}";
-        //}
-
-        public string BuildConnectionString(string domainName)  
-        {       
-            var dataSource = ".";          
-
-            var initialCatalog = domainName;
-
-            return $"Data Source={dataSource};Initial Catalog={initialCatalog};Integrated Security = True;TrustServerCertificate=True";
-        
+           return $"Data Source={dataSource};Initial Catalog={initialCatalog};User ID={userId};Password={password};{trustServerCertificate}";
         }
+
+        // public string BuildConnectionString(string domainName)  
+        // {       
+        //     var dataSource = ".";          
+
+        //     var initialCatalog = domainName;
+
+        //     return $"Data Source={dataSource};Initial Catalog={initialCatalog};Integrated Security = True;TrustServerCertificate=True";
+        
+        // }
     }
 }
